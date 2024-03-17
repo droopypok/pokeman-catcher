efd26134f7162dc9c68f22d8512bb01dc2df9071
droopypok <158001887+droopypok@users.noreply.github.com>
2024-03-17 12:58:40 +0800

    add type inputs and changed username input to display in select avatar

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,28 +2,39 @@ import React, { useRef, useState } from "react";
 
 const Profile = () => {
   const [ppSrc, setPpSrc] = useState("../ProfilePictures/AshPP.png");
+  const [name, setName] = useState(""); // used for displaying name data
   const [region, setRegion] = useState("");
+  const [selectType, setSelectType] = useState([]);
 
-  const nameRef = useRef("");
+  const nameRef = useRef(""); // used to store name data first
 
-  // name fn
+  // name Fn
   const username = (e) => {
     e.preventDefault();
-    console.log(nameRef.current.value);
+    setName(nameRef.current.value);
     nameRef.current.value = "";
   };
 
-  // profile picture fn
+  // profile picture Fn
   const handlePPChange = () => {
     const pp = document.getElementById("profilePicture");
     console.log(pp.value);
     return setPpSrc(pp.value);
   };
 
+  // region select Fn
   const handleRegionChange = () => {
     const region = document.getElementById("regionSelect");
     console.log(region.value);
     return setRegion(region.value);
+  };
+
+  // type select Fn
+  const handleTypeSelect = () => {
+    const type = document.getElementById("typeSelect");
+    const storedType = [...selectType, type.value];
+    setSelectType(storedType);
+    console.log(selectType.join(" "));
   };
 
   return (
@@ -31,12 +42,17 @@ const Profile = () => {
       {/* to enter username -- WORKING */}
       <h3>Enter your name</h3>
       <form onSubmit={username}>
-        <input type="text" placeholder="Enter your name" ref={nameRef} />
+        <input type="text" ref={nameRef} placeholder="Enter your name" />
         <button type="submit">Submit</button>
       </form>
 
       {/* this is the dropdown to select avatar -- WORKING */}
-      <h1>Select your character:</h1>
+      <h2>
+        Select your character:{" "}
+        <span>
+          <h1>{name}</h1>
+        </span>{" "}
+      </h2>
       <img src={ppSrc} alt="" height="300px" />
       <form>
         <select
@@ -56,6 +72,7 @@ const Profile = () => {
 
       {/* selecting region --- WORKING */}
       <h3>Select your region:</h3>
+      <h2>{region}</h2>
       <form>
         <select
           id="regionSelect"
@@ -71,9 +88,12 @@ const Profile = () => {
         </select>
       </form>
 
+      {/* selecting pokeMAN types currently working // Might change to buttons to press instead for UI */}
+      {/* need to add the ability to remove types that selected wrongly  */}
       <h3>Select your preferred PokeMAN type: </h3>
       <form>
-        <select id="typeSelect">
+        <h3>{selectType.join(" ")}</h3>
+        <select id="typeSelect" onChange={handleTypeSelect}>
           <option value="Normal">Normal</option>
           <option value="Fire">Fire</option>
           <option value="Water">Water</option>
