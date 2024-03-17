3f15b75f12ce5f3d5d0fe3d91e9324df1862d6d4
droopypok <158001887+droopypok@users.noreply.github.com>
2024-03-17 12:07:19 +0800

    profile picture select complete with skeleton profile preferences

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,37 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Profile = () => {
-  const pp = document.getElementById("profilePicture");
+  const [ppSrc, setPpSrc] = useState("../ProfilePictures/AshPP.png");
+
   const handlePPChange = () => {
-    return pp.value;
+    const pp = document.getElementById("profilePicture");
+    console.log(pp.value);
+    return setPpSrc(pp.value);
   };
 
   return (
     <div>
-      <img src={handlePPChange} alt="" height="100" width="100" />
-      <select
-        id="profilePicture"
-        onChange={() => {
-          console.log(pp.value);
-        }}
-        defaultValue="Ash"
-      >
-        <option value="https://i.pinimg.com/originals/18/d9/e1/18d9e1307018dbc76750ca7d5124fccd.png">
-          Ash
-        </option>
-        <option value="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cc3b548b-5a7d-4cc1-bb9a-8055970d2965/dbuds4g-e57b1e94-bf9f-4491-8278-79216e1e368a.png/v1/fit/w_768,h_576/misty_render_by_ashleytheskitty_dbuds4g-414w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTc2IiwicGF0aCI6IlwvZlwvY2MzYjU0OGItNWE3ZC00Y2MxLWJiOWEtODA1NTk3MGQyOTY1XC9kYnVkczRnLWU1N2IxZTk0LWJmOWYtNDQ5MS04Mjc4LTc5MjE2ZTFlMzY4YS5wbmciLCJ3aWR0aCI6Ijw9NzY4In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.zN_F9KaX8LzVOpJKG7fO0I-I4XTMctpzkTQ8Ysu2Yus">
-          Misty
-        </option>
-        <option value="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/000157ae-9e27-48d7-a724-cd69a543573f/dfv15o9-ce2d4aa6-3858-4472-bc8d-adf231893609.jpg/v1/fit/w_600,h_600,q_70,strp/brock__pokemon_by_retratosanime_dfv15o9-375w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjAwIiwicGF0aCI6IlwvZlwvMDAwMTU3YWUtOWUyNy00OGQ3LWE3MjQtY2Q2OWE1NDM1NzNmXC9kZnYxNW85LWNlMmQ0YWE2LTM4NTgtNDQ3Mi1iYzhkLWFkZjIzMTg5MzYwOS5qcGciLCJ3aWR0aCI6Ijw9NjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0._yjVRKYUEhiu5hQm2O-RTHVwuMPRNJDz4VxDxCEmVWQ">
-          Brock
-        </option>
-        <option value="https://s1.zerochan.net/Kojirou.%28Pok%C3%A9mon%29.600.3064059.jpg">
-          James
-        </option>
-        <option value="https://d.furaffinity.net/art/imposterdude/1610951885/1610951885.imposterdude_jessie_2.png">
-          Jessie
-        </option>
-      </select>
+      {/* this is the dropdown to select avatar */}
+      <h1>Select your character:</h1>
+      <img src={ppSrc} alt="" height="300px" />
+      <form>
+        <select
+          id="profilePicture"
+          onChange={() => {
+            handlePPChange();
+          }}
+          value={ppSrc}
+        >
+          <option value="../ProfilePictures/AshPP.png">Ash</option>
+          <option value="../ProfilePictures/MistyPP.png">Misty</option>
+          <option value="../ProfilePictures/BrockPP.jpeg">Brock</option>
+          <option value="../ProfilePictures/JamesPP.jpg">James</option>
+          <option value="../ProfilePictures/JessiePP.png">Jessie</option>
+        </select>
+      </form>
+
+      <h3>Select your region:</h3>
+      <form>
+        <select id="regionSelect">
+          <option value="Kanto">Kanto</option>
+          <option value="Johto">Johto</option>
+          <option value="Hoenn">Hoenn</option>
+          <option value="Sinnoh">Sinnoh</option>
+          <option value="Alola">Alola</option>
+        </select>
+      </form>
+
+      <h3>Select your preferred PokeMAN type: </h3>
+      <form>
+        <select id="typeSelect">
+          <option value="Normal">Normal</option>
+          <option value="Fire">Fire</option>
+          <option value="Water">Water</option>
+          <option value="Electric">Electric</option>
+          <option value="Grass">Grass</option>
+          <option value="Ice">Ice</option>
+          <option value="Fighting">Fighting</option>
+          <option value="Poison">Poison</option>
+          <option value="Ground">Ground</option>
+          <option value="Flying">Flying</option>
+          <option value="Psychic">Psychic</option>
+          <option value="Bug">Bug</option>
+          <option value="Rock">Rock</option>
+          <option value="Ghost">Ghost</option>
+          <option value="Dragon">Dragon</option>
+          <option value="Dark">Dark</option>
+          <option value="Steel">Steel</option>
+          <option value="Fairy">Fairy</option>
+        </select>
+      </form>
     </div>
   );
 };
